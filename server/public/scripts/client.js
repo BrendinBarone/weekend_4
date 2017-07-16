b7521eb060158be23ac15e1c0081fa135fec88f6
Brendin Barone <robotcityhq@gmail.com>
2017-07-15 22:30:50 -0500

    pics displays in rows of three, like button added to each

---
+++ server/public/scripts/client.js
@@ -0,0 +1,11 @@
+console.log('js is sourced');
+var myApp = angular.module('myApp', []);
+
+myApp.controller('WorldController', function() {
+  var world = this;
+  world.title = "Brendin's World";
+
+  
+
+
+});
